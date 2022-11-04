4f700e2533635f2027cddc0601824f039e39054c
jiangzhongxun <1215463772@qq.com>
2022-11-05 00:16:33 +0800

    新增 cloud-config-3366

--- cloud-config-client-3366/src/main/java/com/atguigu/springcloud/ConfigClientMain3366.java
+++ cloud-config-client-3366/src/main/java/com/atguigu/springcloud/ConfigClientMain3366.java
@@ -1,5 +1,19 @@
-package com.atguigu.springcloud;/**
-@author jiang zhongxun
-@create 2022-11-04 23:07 
-*/public class ConfigClientMain3366 {
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author jiang zhongxun
+ * @create 2022-11-04 23:07
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class ConfigClientMain3366 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3366.class, args);
+    }
+
 }
--- cloud-config-client-3366/src/main/java/com/atguigu/springcloud/controller/ConfigClientController.java
+++ cloud-config-client-3366/src/main/java/com/atguigu/springcloud/controller/ConfigClientController.java
@@ -1,5 +1,27 @@
-package com.atguigu.springcloud.controller;/**
-@author jiang zhongxun
-@create 2022-11-04 23:11 
-*/public class ConfigClientController {
+package com.atguigu.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author jiang zhongxun
+ * @create 2022-11-04 23:11
+ */
+@RestController
+@RefreshScope
+public class ConfigClientController {
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/configInfo")
+    public String configInfo() {
+        return "serverPort: " + serverPort + "\t\n\n" + "configInfo: " + configInfo;
+    }
+
 }
