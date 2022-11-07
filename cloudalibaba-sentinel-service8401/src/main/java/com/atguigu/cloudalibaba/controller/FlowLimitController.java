a165e036e4eaa9542654cc940453e6c86e26295c
jiangzhongxun <1215463772@qq.com>
2022-11-07 15:54:26 +0800

    更新 8401

--- cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/cloudalibaba/controller/FlowLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/atguigu/cloudalibaba/controller/FlowLimitController.java
@@ -1,5 +1,7 @@
 package com.atguigu.cloudalibaba.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -46,9 +48,17 @@ public class FlowLimitController {
     }
 
     @GetMapping("/testHotKey")
+    @SentinelResource(value = "testHotKey",blockHandler = "deal_testHotKey")
     public String testHotKey(@RequestParam(value = "p1", required = false) String p1,
                              @RequestParam(value = "p2", required = false) String p2) {
         return "------testHotKey" ;
+    }
+
+    /**
+     * 兜底的方法
+     */
+    public String deal_testHotKey(String p1, String p2, BlockException exception) {
+        return "------testHotKey o(╥﹏╥)o，o(╥﹏╥)o";  // sentinel 系统默认的提示：Blocked by sentinel (flow limiting)
     }
 
 }
